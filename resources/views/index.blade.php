4ba39742d8df135726de48550c926d4bb29ab796
jwdeitch <jwdeitch@gmail.com>
2016-02-07 18:32:15 -0500

    styling

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,13 +6,93 @@
     <script src="/js/jquery-2.2.0.min.js"></script>
     <script src="/js/vue.js"></script>
     <link href="/css/bulma.min.css" rel="stylesheet">
+    <link href="/css/css.css" rel="stylesheet">
 
 </head>
 <body>
 
 <div class="container">
+    <h1 class="title">Systems</h1>
 
-    <h1>SysBro</h1>
+    <div class="serverStats">
+        <div class="server">
+            <div class="message">
+                <div class="message-header">
+                    Hello World
+                </div>
+                <div class="message-body">
+                    <div class="columns">
+                        <div class="column stat">
+                            <div class="statTitle">First column</div>
+                            <div class="statValue">First Val</div>
+                        </div>
+                        <div class="column stat">
+                            <div class="statTitle">First column</div>
+                            <div class="statValue">First Val</div>
+                        </div>
+                        <div class="column stat">
+                            <div class="statTitle">First column</div>
+                            <div class="statValue">First Val</div>
+                        </div>
+                        <div class="column stat">
+                            <div class="statTitle">First column</div>
+                            <div class="statValue">First Val</div>
+                        </div>
+                        <div class="column stat">
+                            <div class="statTitle">First column</div>
+                            <div class="statValue">First Val</div>
+                        </div>
+                        <div class="column stat">
+                            <div class="statTitle">First column</div>
+                            <div class="statValue">First Val</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="server">
+            <div class="message">
+                <div class="message-header">
+                    Hello World
+                </div>
+                <div class="message-body">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Pellentesque risus mi, tempus quis placerat ut, porta nec
+                    nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam
+                    gravida purus diam, et dictum felis venenatis efficitur.
+                    Aenean ac eleifend lacus, in mollis lectus. Donec sodales,
+                    arcu et sollicitudin porttitor, tortor urna tempor ligula,
+                    id porttitor mi magna a neque. Donec dui urna, vehicula et
+                    sem eget, facilisis sodales sem.
+                </div>
+            </div>
+        </div>
+        <div class="server">
+            <div class="message">
+                <div class="message-header">
+                    Hello World
+                </div>
+                <div class="message-body">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Pellentesque risus mi, tempus quis placerat ut, porta nec
+                    nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam
+                    gravida purus diam, et dictum felis venenatis efficitur.
+                    Aenean ac eleifend lacus, in mollis lectus. Donec sodales,
+                    arcu et sollicitudin porttitor, tortor urna tempor ligula,
+                    id porttitor mi magna a neque. Donec dui urna, vehicula et
+                    sem eget, facilisis sodales sem.
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="analytics">
+
+    </div>
+
+    <div class="errorLog">
+
+    </div>
 
 </div>
 
