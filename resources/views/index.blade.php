39de050446b0caadbee2d1866a3579846d381773
jwdeitch <jwdeitch@gmail.com>
2016-02-07 17:02:50 -0500

    routes

---
+++ resources/views/index.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>SystemBro</title>
+    <script src="/js/jquery-2.2.0.min.js"></script>
+    <script src="/js/vue.js"></script>
+    <link href="/css/bulma.min.css" rel="stylesheet">
+
+</head>
+<body>
+
+<div class="container">
+
+    <h1>SysBro</h1>
+
+</div>
+
+
+
+</body>
+</html>
