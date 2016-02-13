4ff309346e992bd9cf7631d396b5bd041f9aceae
jwdeitch <jwdeitch@gmail.com>
2016-02-13 11:43:05 -0500

    fronend progress

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -29,56 +29,86 @@
                 up @{{collectionItem.uptime}}
             </div>
             <hr>
-
             <div class="mainContent">
-                <div class="ui tiny statistics">
-                    <div class="statistic">
-                        <div class="value">
-                            @{{collectionItem.cpu1min}}
+
+                <div class="stats">
+                    <div class="ui tiny statistics">
+                        <div class="statistic">
+                            <div class="value">
+                                @{{collectionItem.cpu1min}}
+                            </div>
+                            <div class="label">
+                                CPU activity
+                            </div>
                         </div>
-                        <div class="label">
-                            CPU activity
+                        <div class="statistic">
+                            <div class="value">
+                                @{{collectionItem.formatted.memPercent}}
+                            </div>
+                            <div class="label">
+                                RAM usage
+                            </div>
                         </div>
-                    </div>
-                    <div class="statistic">
-                        <div class="value">
-                            @{{collectionItem.formatted.memPercent}}
+                        <div class="statistic">
+                            <div class="value">
+                                @{{collectionItem.formatted.diskPercent}}
+                            </div>
+                            <div class="label">
+                                used disk space
+                            </div>
                         </div>
-                        <div class="label">
-                            RAM usage
+                        <div class="statistic">
+                            <div class="value">
+                                @{{collectionItem.activeSsh}}
+                            </div>
+                            <div class="label">
+                                SSH session(s)
+                            </div>
                         </div>
-                    </div>
-                    <div class="statistic">
-                        <div class="value">
-                            @{{collectionItem.formatted.diskPercent}}
-                        </div>
-                        <div class="label">
-                            used disk space
-                        </div>
-                    </div>
-                    <div class="statistic">
-                        <div class="value">
-                            @{{collectionItem.activeSsh}}
-                        </div>
-                        <div class="label">
-                            SSH session(s)
-                        </div>
-                    </div>
-                    <div class="statistic">
-                        <div class="value">
-                            @{{collectionItem.formatted.bandwidth.month}}
-                        </div>
-                        <div class="label">
-                            bandwidth usage this month
+                        <div class="statistic">
+                            <div class="value">
+                                @{{collectionItem.formatted.bandwidth.month}}
+                            </div>
+                            <div class="label">
+                                bandwidth usage this month
+                            </div>
                         </div>
                     </div>
                 </div>
 
-            </div>
 
+                <div class="ui grid">
+                    <div class="twelve wide column">
+                        <table class="ui very compact single line selectable table">
+                            <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Status</th>
+                                <th>Another Status</th>
+                                <th>Notes</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>John</td>
+                                <td>Approved</td>
+                                <td>Approved</td>
+                                <td>None</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="four wide column">
+                        wefjnwefkjwnefkjwenfnc mcwemn wenfwe febwnf
+                    </div>
+                </div>
+
+            </div>
         </div>
 
     </div>
+
+</div>
 
 </div>
 
