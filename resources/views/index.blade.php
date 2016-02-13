b611bc68961cda462167e215855b72b8b720013e
jwdeitch <jwdeitch@gmail.com>
2016-02-13 12:45:46 -0500

    fronend progress

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -79,27 +79,127 @@
 
                 <div class="ui grid">
                     <div class="twelve wide column">
-                        <table class="ui very compact single line selectable table">
+                        <table class="ui very compact selectable table">
                             <thead>
                             <tr>
-                                <th>Name</th>
-                                <th>Status</th>
-                                <th>Another Status</th>
-                                <th>Notes</th>
+                                <th>IP</th>
+                                <th>Visited</th>
+                                <th>Request</th>
+                                <th>OS</th>
+                                <th>Browser</th>
+                                <th>Country</th>
+                                <th>City</th>
+                                <th>Total Visits</th>
+                                <th>Last HTTP resp.</th>
                             </tr>
                             </thead>
                             <tbody>
-                            <tr>
-                                <td>John</td>
-                                <td>Approved</td>
-                                <td>Approved</td>
-                                <td>None</td>
+                            <tr v-for="visitor in collectionItem.analytics.recentVisitors">
+                                <td>@{{visitor._id}}</td>
+                                <td>@{{visitor.time}}</td>
+                                <td>@{{visitor.requestedPage}}</td>
+                                <td>@{{visitor.device.platform}}</td>
+                                <td>@{{visitor.device.browser}}</td>
+                                <td>@{{visitor.location.country}}</td>
+                                <td>@{{visitor.location.city}}</td>
+                                <td>@{{visitor.count}}</td>
+                                <td>@{{visitor.status}}</td>
                             </tr>
                             </tbody>
                         </table>
                     </div>
                     <div class="four wide column">
-                        wefjnwefkjwnefkjwenfnc mcwemn wenfwe febwnf
+                        <div class="statUnit">
+                            <div class="title">
+                                Unique visits this...
+                            </div>
+                            <div class="body">
+                                <div class="ui mini statistics">
+                                    <div class="statistic">
+                                        <div class="label">
+                                            day
+                                        </div>
+                                        <div class="value">
+                                            @{{collectionItem.analytics.uniqueVisits.day}}
+                                        </div>
+                                    </div>
+                                    <div class="statistic">
+                                        <div class="label">
+                                            week
+                                        </div>
+                                        <div class="value">
+                                            @{{collectionItem.analytics.uniqueVisits.week}}
+                                        </div>
+                                    </div>
+                                    <div class="statistic">
+                                        <div class="label">
+                                            month
+                                        </div>
+                                        <div class="value">
+                                            @{{collectionItem.analytics.uniqueVisits.month}}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="statUnit">
+                            <div class="title">
+                                Total requests this...
+                            </div>
+                            <div class="body">
+                                <div class="ui mini statistics">
+                                    <div class="statistic">
+                                        <div class="label">
+                                            day
+                                        </div>
+                                        <div class="value">
+                                            @{{collectionItem.analytics.totalRequestCount.day}}
+                                        </div>
+                                    </div>
+                                    <div class="statistic">
+                                        <div class="label">
+                                            week
+                                        </div>
+                                        <div class="value">
+                                            @{{collectionItem.analytics.totalRequestCount.week}}
+                                        </div>
+                                    </div>
+                                    <div class="statistic">
+                                        <div class="label">
+                                            month
+                                        </div>
+                                        <div class="value">
+                                            @{{collectionItem.analytics.totalRequestCount.month}}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="statUnit">
+                            <div class="title">
+                                Bandwidth used this...
+                            </div>
+                            <div class="body">
+                                <div class="ui mini statistics">
+                                    <div class="statistic">
+                                        <div class="label">
+                                            day
+                                        </div>
+                                        <div class="value">
+                                            @{{collectionItem.formatted.bandwidth.day}}
+                                        </div>
+                                    </div>
+                                    <div class="statistic">
+                                        <div class="label">
+                                            week
+                                        </div>
+                                        <div class="value">
+                                            @{{collectionItem.formatted.bandwidth.week}}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
